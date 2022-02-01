0d913f6e299d0b7dfccb88cf3ebac4780c546419
moharan627 <74227850+moharan627@users.noreply.github.com>
2022-02-01 21:04:08 +0900

    Add files via upload

---
+++ Comb.py
@@ -0,0 +1,12 @@
+class Comb(limit = 200_020,_mod = 10**9+7):
+    def __init__(self, limit:int,_mod:int):
+        self.mod = _mod
+        self.Fact = [0] * limit
+        self.Fact[0] = 1
+        self.Fact[1] = 1
+        for n in range(2, limit):
+            self.Fact[n] = (n * (self.Fact[n - 1])) % self.mod
+    def __modinv__(self,x):
+        return pow(x, self.mod - 2, self.mod)
+    def nCk(self,n:int,k:int):
+        return self.Fact[n] * self.modinv(self.Fact[n-k] * self.Fact[k])
